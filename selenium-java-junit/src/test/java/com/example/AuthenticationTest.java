63ddfb38ddc58124a49b6c5d202366e5c8c3a896
Sommai-Supawong <Somai.050449@gmail.com>
2026-09-17 11:47:27 +0700

    feat(auth):add 12 test case authentication test

--- selenium-java-junit/src/test/java/com/example/AuthenticationTest.java
+++ selenium-java-junit/src/test/java/com/example/AuthenticationTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-class LoginTest {
+class AuthenticationTest {
 
     private WebDriver driver;
 
